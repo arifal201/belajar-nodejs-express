bdfcc8b448b7d9b58a96703357cfcf6154f07a05
arifal201 <arifalhidayat10@gmail.com>
2023-02-22 23:53:10 +0700

    finished course nodejs

--- test/request-body.test.js
+++ test/request-body.test.js
@@ -1,9 +1,11 @@
-import express from "express";
+import express, { text } from "express";
 import request from "supertest";
+import expressFileUpload from "express-fileupload";
 
 const app = express();
   app.use(express.json());
   app.use(express.urlencoded({extended: false}));
+  app.use(expressFileUpload());
 
 app.post('/json', (req,res) => {
   const name = req.body.name;
@@ -21,6 +23,23 @@ app.post('/form', (req,res) => {
       message: `Hello ${name}`
     }
   );
+});
+
+app.post("/file", async (req,res) => {
+  const textFile = req.files.article;
+  await textFile.mv(__dirname + "/upload/" + textFile.name);
+
+  res.send(`Hello ${req.body.name}, you uploaded ${textFile.name}`);
+});
+
+test("test request file upload", async () =>{
+  const response = await request(app)
+  .post("/file")
+  .set("content-type", "multipart/form-data")
+  .field("name", "Arifal")
+  .attach("article", __dirname + "/sample.txt");
+
+  expect(response.text).toBe("Hello Arifal, you uploaded sample.txt");
 });
 
 test("test request json", async () =>{
--- test/template.test.js
+++ test/template.test.js
@@ -4,11 +4,20 @@ import mustacheExpress from "mustache-express";
 
 const app = express();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.engine('html', mustacheExpress());
+
 app.get('/', (req,res) => {
-  res.send('hello world');
+  res.render('index', {
+    title: 'test render template',
+    say: 'hello world'
+  })
 });
 
-test("test express", async () =>{
+test("test render template", async () =>{
   const response = await request(app).get("/");
-  expect(response.text).toBe("hello world");
+  console.info(response.text);
+  expect(response.text).toContain('test render template');
+  expect(response.text).toContain("hello world");
 });
